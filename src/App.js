85be6d052d6b739fe46900e7c0ff37c3dc117907
bamsemats <mats.f.ronnqvist@gmail.com>
2025-05-14 23:36:53 +0200

    Play around and get stuck in rabbit holes

--- src/App.js
+++ src/App.js
@@ -41,6 +41,7 @@ function handleMouseEnter(e) {
     const randomColor = Math.floor(Math.random() * 999999);
     const randomColor2 = Math.floor(Math.random() * 999999);
     console.log(randomColor);
+    console.log(randomColor2)
     hoveredElement.style.setProperty('--random-color', `#${randomColor}`);
     hoveredElement.style.setProperty('--random-color-2', `#${randomColor2}`);
   }
@@ -51,16 +52,57 @@ function handleMouseEnter(e) {
 function App() {
   return (
     <div className="App">
+      <form className='color-picker' action=''>
+        <fieldset>
+          <legend className='visually-hidden'>Pick a color scheme</legend>
+          <label htmlFor='light' className='visually-hidden'>Light Theme</label>
+          <input 
+            type='radio' 
+            name='theme' 
+            id='light'
+            defaultChecked
+          >
+          </input>
+          <label htmlFor='dark' className='visually-hidden'>Dark Theme</label>
+          <input 
+            type='radio' 
+            name='theme' 
+            id='dark'
+          >
+          </input>
+          <label htmlFor='pink' className='visually-hidden'>Pink Theme</label>
+          <input 
+            type='radio' 
+            name='theme' 
+            id='pink'
+          >
+          </input>
+          <label htmlFor='blue' className='visually-hidden'>Blue Theme</label>
+          <input 
+            type='radio' 
+            name='theme' 
+            id='blue'
+          >
+          </input>
+          <label htmlFor='green' className='visually-hidden'>Green Theme</label>
+          <input 
+            type='radio' 
+            name='theme' 
+            id='green'
+          >
+          </input>
+        </fieldset>
+      </form>
       <div className='svg-container'>
         <button className='button-one' onClick={handleClickButtonOne}>
-          <svg fill='var(--color-button)' className='hamburger' viewBox='0 0 100 100' width='250'>
+          <svg fill='var(--color-button-one)' className='hamburger' viewBox='0 0 100 100' width='250'>
             <rect className='line top' width='80' height='10' x='10' y='25' rx='5'></rect>
             <rect className='line middle' width='80' height='10' x='10' y='45' rx='5'></rect>
             <rect className='line bottom' width='80' height='10' x='10' y='65' rx='5'></rect>
           </svg>
         </button>
          <button className='button-two' onClick={handleClickButtonTwo}>
-          <svg fill='var(--color-button)' className='hamburger' viewBox='0 0 100 100' width='250'>
+          <svg fill='var(--color-button-two)' className='hamburger' viewBox='0 0 100 100' width='250'>
             <rect className='line top1' width='80' height='10' x='10' y='25' rx='5'></rect>
             <rect className='line top2' width='80' height='10' x='10' y='25' rx='5'></rect>
             <rect className='line middle' width='80' height='10' x='10' y='45' rx='5'></rect>
@@ -69,7 +111,7 @@ function App() {
           </svg>
         </button>
         <button className='button-three' onClick={handleClickButtonThree}>
-          <svg stroke='var(--color-button)' fill='none' className='hamburger' viewBox='-10 -10 120 120' width='250'>
+          <svg stroke='var(--color-button-three)' fill='none' className='hamburger' viewBox='-10 -10 120 120' width='250'>
             <path className='line' strokeWidth='10' strokeLinecap='round' strokeLinejoin='round' d='m 14 70 l 70 0 a 1 1 0 0 0 0 -20 l -70 0 a 1 1 0 0 1 0 -20 l 70 0 a 1 1 0 0 0 -35 -20 l 0 76'></path>
           </svg>
         </button>
@@ -100,16 +142,16 @@ function App() {
           <a href='#' style={{'--sibling-index': 10}}><span>10</span>Clementine</a>
         </div>
         <div className='animated-list-container-three'>
-          <span><a href='#' style={{'--sibling-index': 1}} className='span1' onMouseEnter={() =>handleMouseEnter('span1')}><span>01</span>Banana</a></span>
-          <span><a href='#' style={{'--sibling-index': 2}} className='span2' onMouseEnter={() =>handleMouseEnter('span2')}><span>02</span>Pear</a></span>
-          <span><a href='#' style={{'--sibling-index': 3}} className='span3' onMouseEnter={() =>handleMouseEnter('span3')}><span>03</span>Orange</a></span>
-          <span><a href='#' style={{'--sibling-index': 4}} className='span4' onMouseEnter={() =>handleMouseEnter('span4')}><span>04</span>Apple</a></span>
-          <span><a href='#' style={{'--sibling-index': 5}} className='span5' onMouseEnter={() =>handleMouseEnter('span5')}><span>05</span>Melon</a></span>
-          <span><a href='#' style={{'--sibling-index': 6}} className='span6' onMouseEnter={() =>handleMouseEnter('span6')}><span>06</span>Grape</a></span>
-          <span><a href='#' style={{'--sibling-index': 7}} className='span7' onMouseEnter={() =>handleMouseEnter('span7')}><span>07</span>Mango</a></span>
-          <span><a href='#' style={{'--sibling-index': 8}} className='span8' onMouseEnter={() =>handleMouseEnter('span8')}><span>08</span>Peach</a></span>
-          <span><a href='#' style={{'--sibling-index': 9}} className='span9' onMouseEnter={() =>handleMouseEnter('span9')}><span>09</span>Lemon</a></span>
-          <span><a href='#' style={{'--sibling-index': 10}} className='span10' onMouseEnter={() =>handleMouseEnter('span10')}><span>10</span>Clementine</a></span>
+          <div><a href='#' style={{'--sibling-index': 1}} className='span1' onMouseEnter={() =>handleMouseEnter('span1')}><span className='inner-span'>01</span>Banana</a></div>
+          <span><a href='#' style={{'--sibling-index': 2}} className='span2' onMouseEnter={() =>handleMouseEnter('span2')}><span className='inner-span'>02</span>Pear</a></span>
+          <span><a href='#' style={{'--sibling-index': 3}} className='span3' onMouseEnter={() =>handleMouseEnter('span3')}><span className='inner-span'>03</span>Orange</a></span>
+          <span><a href='#' style={{'--sibling-index': 4}} className='span4' onMouseEnter={() =>handleMouseEnter('span4')}><span className='inner-span'>04</span>Apple</a></span>
+          <span><a href='#' style={{'--sibling-index': 5}} className='span5' onMouseEnter={() =>handleMouseEnter('span5')}><span className='inner-span'>05</span>Melon</a></span>
+          <span><a href='#' style={{'--sibling-index': 6}} className='span6' onMouseEnter={() =>handleMouseEnter('span6')}><span className='inner-span'>06</span>Grape</a></span>
+          <span><a href='#' style={{'--sibling-index': 7}} className='span7' onMouseEnter={() =>handleMouseEnter('span7')}><span className='inner-span'>07</span>Mango</a></span>
+          <span><a href='#' style={{'--sibling-index': 8}} className='span8' onMouseEnter={() =>handleMouseEnter('span8')}><span className='inner-span'>08</span>Peach</a></span>
+          <span><a href='#' style={{'--sibling-index': 9}} className='span9' onMouseEnter={() =>handleMouseEnter('span9')}><span className='inner-span'>09</span>Lemon</a></span>
+          <span><a href='#' style={{'--sibling-index': 10}} className='span10' onMouseEnter={() =>handleMouseEnter('span10')}><span className='inner-span'>10</span>Clementine</a></span>
         </div>
       </div>
     </div>
